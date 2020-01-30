c6c3e32d4e781cfcdc9ae48f5028e92bd8659c01
sh <2662419405@qq.com>
2020-01-30 23:51:34 +0800

    :tada: 取消sass

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
+import './index.less';
 import App from './route';
 
 ReactDOM.render(<App />, document.getElementById('root'));
