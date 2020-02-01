14a5a2b4be8387bc06e57ba4e6afc5a1012a20f9
孙航 <2662419405@qq.com>
2020-02-01 23:09:16 +0800

    :tada: 修改

--- src/component/Home/index.js
+++ src/component/Home/index.js
@@ -1,11 +1,14 @@
 import React, { PureComponent } from "react";
+import Banner from "../banner";
 import "./index.less";
 
 class Home extends PureComponent {
   render() {
     return (
-      <div>
-          123112312321
+      <div className="page index">
+        <div className="index-container">
+          <Banner />
+        </div>
       </div>
     );
   }
---
+++ src/component/banner/index.js
@@ -0,0 +1,30 @@
+import React, { PureComponent } from "react";
+import "./index.less";
+
+class Home extends PureComponent {
+  render() {
+    return (
+      <div className="banner-container clearfix">
+        <div className="left-banner">
+          <div className="category-nav-container">
+            <div className="category-nav-title-wrapper">
+              <span className="category-nav-title">全部分类</span>
+            </div>
+            <div className="category-nav-content-wrapper">
+              <ul>
+                <li>
+                  
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="right-banner">
+          
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
