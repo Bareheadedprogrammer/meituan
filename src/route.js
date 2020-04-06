16177ade960edcb6448fb8c7735cc93bec69cc40
孙航 <2662419405@qq.com>
2020-04-06 08:59:39 +0800

    :fire:使用代价分割

--- src/route.js
+++ src/route.js
@@ -1,18 +1,19 @@
 import React, { PureComponent } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { Provider } from "react-redux";
+import store from "./store/store";
+import loadable from "@loadable/component";
 import Home from "./component/Home";
 import Error from "./component/404";
 import Register from "./common/Register";
-import Login from "./common/Login";
 import Search from "./component/search";
 import Meishi from "./component/detail/meishi";
 import Cate from "./component/detail/cate";
 import Jiudian from "./component/detail/jiudian";
 import ScrollToTop from "./ScrollToTop";
-import Zhao from "./common/zhao";
 import App from "./app";
-import store from "./store/store";
+const Login = loadable(() => import("./common/Login"));
+const Zhao = loadable(()=>import("./common/zhao"));
 
 // 路由配置
 export default class route extends PureComponent {
